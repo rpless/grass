c0a2279ab965eb82b57503026c8554e916f8d800
Ryan Plessner <plessner.ryan@gmail.com>
2014-01-28 09:53:50 -0500

    documented the SimulationWindow. Also changed it to explicitly use the GL4 Profile

--- src/main/java/rpless/grass/window/SimulationWindow.java
+++ src/main/java/rpless/grass/window/SimulationWindow.java
@@ -11,9 +11,19 @@ import javax.media.opengl.GLCapabilities;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GLProfile;
 
+/**
+ * The {@code SimulationWindow} encapsulates the NEWT window that from JOGL.
+ * <p>
+ *     It can be started and stopped.
+ */
 public class SimulationWindow {
     public static final Float WIDTH = 800.0f;
     public static final Float HEIGHT = 600.0f;
+
+    /**
+     * The {@code SimulationWindowListener} is a {@link com.jogamp.newt.event.WindowAdapter} that stops the
+     * {@link com.jogamp.opengl.util.AnimatorBase} on a window destroy notification.
+     */
     private class SimulationWindowListener extends WindowAdapter {
         @Override
         public void windowDestroyNotify(WindowEvent windowEvent) {
@@ -25,7 +35,7 @@ public class SimulationWindow {
     private AnimatorBase animatorBase;
 
     public SimulationWindow() {
-        GLProfile profile = GLProfile.getDefault();
+        GLProfile profile = GLProfile.get(GLProfile.GL4);
         System.out.println(profile);
         GLCapabilities capabilities = new GLCapabilities(profile);
         window = GLWindow.create(capabilities);
@@ -35,16 +45,27 @@ public class SimulationWindow {
         window.addWindowListener(new SimulationWindowListener());
     }
 
+    /**
+     *
+     * @param keyListener
+     */
     public void addKeyListener(KeyListener keyListener) {
         window.addKeyListener(keyListener);
     }
 
+    /**
+     * Start the {@link com.jogamp.opengl.util.AnimatorBase} with the given {@link javax.media.opengl.GLEventListener}.
+     * @param eventListener The {@link javax.media.opengl.GLEventListener} to use
+     */
     public void start(GLEventListener eventListener) {
         window.addGLEventListener(eventListener);
         animatorBase = new FPSAnimator(window, 60, true);
         animatorBase.start();
     }
 
+    /**
+     * Destroys the window.
+     */
     public void stop() {
         window.destroy();
     }

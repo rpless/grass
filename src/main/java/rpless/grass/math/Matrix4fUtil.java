a2a46e6cfa2f387cc2df2489762e754dc55a1086
Ryan Plessner <plessner.ryan@gmail.com>
2014-01-29 18:06:58 -0500

    added rotation helpers

--- src/main/java/rpless/grass/math/Matrix4fUtil.java
+++ src/main/java/rpless/grass/math/Matrix4fUtil.java
@@ -10,6 +10,68 @@ public class Matrix4fUtil {
         return mat;
     }
 
+    public static Matrix4f rotateX(float angle) {
+        float theta = (float) Math.toRadians(angle);
+        float cosTheta = (float) Math.cos(theta);
+        float sinTheta = (float) Math.sin(theta);
+        Matrix4f rot = Matrix4f.identity();
+
+        rot.set(1, 1, cosTheta);
+        rot.set(2, 1, -sinTheta);
+        rot.set(1, 2, sinTheta);
+        rot.set(2, 2, cosTheta);
+        return rot;
+    }
+
+    public static Matrix4f rotateY(float angle) {
+        float theta = (float) Math.toRadians(angle);
+        float cosTheta = (float) Math.cos(theta);
+        float sinTheta = (float) Math.sin(theta);
+        Matrix4f rot = Matrix4f.identity();
+        rot.set(0, 0, cosTheta);
+        rot.set(2, 0, sinTheta);
+        rot.set(0, 2, -sinTheta);
+        rot.set(2, 2, cosTheta);
+        return rot;
+    }
+
+    public static Matrix4f rotateZ(float angle) {
+        float theta = (float) Math.toRadians(angle);
+        float cosTheta = (float) Math.cos(theta);
+        float sinTheta = (float) Math.sin(theta);
+        Matrix4f rot = Matrix4f.identity();
+        rot.set(0, 0, cosTheta);
+        rot.set(1, 0, -sinTheta);
+        rot.set(0, 1, sinTheta);
+        rot.set(1, 1, cosTheta);
+        return rot;
+    }
+
+    public static Matrix4f rotate(Vector3f axis, float angle) {
+        Vector3f nAxis = axis.normalize();
+        float rAngle = (float) Math.toRadians(angle);
+        float fCos = (float) Math.cos(rAngle);
+        float fInvCos = 1 - fCos;
+        float fSin = (float) Math.sin(rAngle);
+        float xSquared = nAxis.x() * nAxis.x();
+        float ySquared = nAxis.y() * nAxis.y();
+        float zSquared = nAxis.z() * nAxis.z();
+
+        Matrix4f rot = Matrix4f.identity();
+        rot.set(0, 0, xSquared + ((1 - xSquared) * fCos));
+        rot.set(0, 1, (nAxis.x() * nAxis.y() * fInvCos + (nAxis.z() * fSin)));
+        rot.set(0, 2, nAxis.x() * nAxis.z() * fInvCos + (nAxis.y() * fSin));
+
+        rot.set(1, 0, nAxis.x() * nAxis.y() * fInvCos - (nAxis.z() * fSin));
+        rot.set(1, 1, ySquared + ((1 - ySquared) * fCos));
+        rot.set(1, 2, nAxis.y() * nAxis.z() * fInvCos + (nAxis.x() * fSin));
+
+        rot.set(2, 0, nAxis.x() * nAxis.z() * fInvCos + (nAxis.y() * fSin));
+        rot.set(2, 1, nAxis.y() * nAxis.z() * fInvCos - (nAxis.x() * fSin));
+        rot.set(2, 2, zSquared + ((1 - zSquared) * fCos));
+        return rot;
+    }
+
     public static Matrix4f perspective(float fov, float aspect, float zNear, float zFar) {
         float scale = (float) Math.tan(Math.toRadians(fov / 2)) * zNear;
         float r = aspect * scale;

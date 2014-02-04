e99f5984e76732ebdfb2533b59648cd57b0946e3
Ryan Plessner <plessner.ryan@gmail.com>
2014-02-03 21:16:00 -0500

    created a commonmath class to provide utility class for math. currently the only method is an implementation of rodrigues' vector rotation algorithm

--- src/main/java/rpless/grass/math/CommonMath.java
+++ src/main/java/rpless/grass/math/CommonMath.java
@@ -1,7 +1,21 @@
 package rpless.grass.math;
 
-/**
- * Created by ryan on 2/3/14.
- */
 public class CommonMath {
+    /**
+     * returns a new vector which is {@code v} rotated around {@code axis} by {@code alpha} degrees clockwise<p>
+     * @param v The vector to rotate
+     * @param axis The axis of rotation
+     * @param alpha The angle of rotation
+     * @return The new vector
+     */
+    public static Vector3f rotate(Vector3f v, Vector3f axis, float alpha)
+    {
+        //This formula is based on Rodrigues' rotation formula.
+        alpha *= -1;
+        Vector3f k = axis.normalize();
+        Vector3f p2 = k.crossProduct(v).multiply((float)Math.sin(alpha));
+        Vector3f p3 = k.multiply(k.dotProduct(v)).multiply((float) (1 - Math.cos(alpha)));
+        Vector3f p1 = v.multiply((float)Math.cos(alpha));
+        return p1.add(p2).add(p3);
+    }
 }

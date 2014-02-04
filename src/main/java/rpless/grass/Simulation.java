4a482fe8b039bbf0a86c3ee7bb17883d4092b17b
Ryan Plessner <plessner.ryan@gmail.com>
2014-02-04 18:37:16 -0500

    got a geometry shader to duplicate the cube 4 units away from the original

--- src/main/java/rpless/grass/Simulation.java
+++ src/main/java/rpless/grass/Simulation.java
@@ -62,7 +62,7 @@ public class Simulation implements GLEventListener {
         GL4 gl = drawable.getGL().getGL4();
         gl.glClear(GL4.GL_COLOR_BUFFER_BIT | GL4.GL_DEPTH_BUFFER_BIT);
         shaderProgram.uniform(gl, "cameraMatrix", camera.lookAt());
-        shaderProgram.uniform(gl, "modelMatrix", Matrix4fUtil.translate(0, 0, -5).multiply(Matrix4fUtil.rotateY(45)));
+        shaderProgram.uniform(gl, "modelMatrix", Matrix4fUtil.translate(-1, 0, -5).multiply(Matrix4fUtil.rotateY(45)));
         mesh.display(gl);
     }
 
--- src/main/resources/vertex.glsl
+++ src/main/resources/vertex.glsl
@@ -3,15 +3,11 @@
 layout(location = 0) in vec4 position;
 layout(location = 1) in vec4 color;
 
-uniform mat4 modelMatrix;
-uniform mat4 cameraMatrix;
-uniform mat4 perspectiveMatrix;
-
 out vertexData {
   vec4 color;
 } vertex;
 
 void main() {
-  gl_Position = perspectiveMatrix * cameraMatrix * modelMatrix * position;
+  gl_Position = position;
   vertex.color = color;
 }

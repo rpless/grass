1cf6908b0490aaf4e9014a99d3badcf29e421d36
Ryan Plessner <plessner.ryan@gmail.com>
2014-02-12 14:22:09 -0500

    Generated a triangle at 'random' point on the triangle. Used barycentric coordinates to get a point on the triangle. Currently the 'uniformly distributed random values' are hard coded.

--- src/main/resources/grass.glsl
+++ src/main/resources/grass.glsl
@@ -1,13 +1,13 @@
 #version 330
 
-layout(triangles) in;
+layout(triangles, invocations = 1) in;
 in vec4 vColor[];
 
 uniform mat4 modelMatrix;
 uniform mat4 cameraMatrix;
 uniform mat4 perspectiveMatrix;
 
-layout(triangle_strip, max_vertices=3) out;
+layout(triangle_strip, max_vertices=6) out;
 
 out vec4 gColor;
 
@@ -17,5 +17,24 @@ void main() {
     gColor = vColor[i];
     EmitVertex();
   }
+  EndPrimitive();
+
+  float R = 0.4;
+  float S = 0.4;
+  vec4 A = gl_in[0].gl_Position;
+  vec4 B = gl_in[1].gl_Position;
+  vec4 C = gl_in[2].gl_Position;
+  vec4 bary = A + (R * (B - A)) + (S * (C - A));
+
+  // Emit coordinates
+  gl_Position = perspectiveMatrix * cameraMatrix * modelMatrix * bary;
+  gColor = vColor[0] + vec4(0.4, 0, 0, 0);
+  EmitVertex();
+  gl_Position = perspectiveMatrix * cameraMatrix * modelMatrix * (bary + vec4(0.1, 0, 0, 0));
+  gColor = vColor[0] + vec4(0.4, 0, 0, 0);
+  EmitVertex();
+  gl_Position = perspectiveMatrix * cameraMatrix * modelMatrix * (bary + vec4(0.1, 0.1, 0, 0));
+  gColor = vColor[0] + vec4(0.4, 0, 0, 0);
+  EmitVertex();
   EndPrimitive();
 }

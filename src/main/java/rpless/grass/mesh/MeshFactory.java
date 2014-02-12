6547a03dd619b3cc75b94ea1611fe88f605ca6b9
Ryan Plessner <plessner.ryan@gmail.com>
2014-02-12 14:19:17 -0500

    found a bug in the mesh factory that was causing the geometry shader to be called more times than it should have been. needed to call glDrawElements on the number of elements in the index buffer, NOT the number in the vertex buffer

--- src/main/java/rpless/grass/mesh/MeshFactory.java
+++ src/main/java/rpless/grass/mesh/MeshFactory.java
@@ -17,6 +17,6 @@ public class MeshFactory {
     public static Mesh createMesh(GL4 gl, FloatBuffer vertexData, ShortBuffer indexData, Collection<MeshFormat> formats) {
         NativeBuffer vertexBuffer = NativeBuffer.createBuffer(gl, vertexData, GL4.GL_ARRAY_BUFFER, GL4.GL_FLOAT, GL4.GL_STATIC_DRAW);
         NativeBuffer indexBuffer = NativeBuffer.createBuffer(gl, indexData, GL4.GL_ELEMENT_ARRAY_BUFFER, GL4.GL_UNSIGNED_SHORT, GL4.GL_STATIC_DRAW);
-        return new Mesh(vertexBuffer, indexBuffer, vertexData.capacity(), formats);
+        return new Mesh(vertexBuffer, indexBuffer, indexData.capacity(), formats);
     }
 }

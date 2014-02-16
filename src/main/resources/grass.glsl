394a67f7a47a38889696f02695d95221d46baa2c
Ryan Plessner <plessner.ryan@gmail.com>
2014-02-16 13:17:27 -0500

    moved all of the helper functions in the geometry shader to the bottom of the file. also created a function to render a single blade of grass

--- src/main/resources/grass.glsl
+++ src/main/resources/grass.glsl
@@ -11,17 +11,10 @@ layout(triangle_strip, max_vertices=15) out;
 
 out vec4 gColor;
 
-float rand(vec2 co){
-  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-}
-
-vec4 bary(float R, float S) {
-  if (R + S >= 1) {
-    R = 1 - R;
-    S = 1 - S;
-  }
-  return gl_in[0].gl_Position + (R * (gl_in[1].gl_Position - gl_in[0].gl_Position)) + (S * (gl_in[2].gl_Position - gl_in[0].gl_Position));
-}
+// Helper Function Signatures
+float rand(vec2 co);
+vec4 bary(float R, float S);
+void grassBlade(vec4 center, mat4 PCMMatrix);
 
 void main() {
   mat4 PCMMatrix = (perspectiveMatrix * cameraMatrix) * modelMatrix; // Precompute the perspective/camera/model matrix
@@ -35,15 +28,19 @@ void main() {
   EndPrimitive();
 
   vec4 coordinate = bary(rand(vec2(40, 20)), rand(vec2(0.4, 0.2)));
-  vec4 A = coordinate + vec4(0, 0, 0.01, 0);
-  vec4 B = coordinate + vec4(0.0025, 0, 0, 0);
-  vec4 C = coordinate - vec4(0, 0, 0.01, 0);
-  vec4 D = coordinate - vec4(0.0025, 0, 0, 0);
-  vec4 E = coordinate + vec4(0, 0.1, 0, 0);
+  grassBlade(coordinate, PCMMatrix);
+}
+
+void grassBlade(vec4 center, mat4 PCMMatrix) {
+  vec4 A = center + vec4(0, 0, 0.01, 0);
+  vec4 B = center + vec4(0.0025, 0, 0, 0);
+  vec4 C = center - vec4(0, 0, 0.01, 0);
+  vec4 D = center - vec4(0.0025, 0, 0, 0);
+  vec4 E = center + vec4(0, 0.1, 0, 0);
 
   // Emit coordinates
   gColor = vColor[0] + vec4(0.4, 0, 0, 0);
-  
+
   gl_Position = PCMMatrix * A;
   EmitVertex();
   gl_Position = PCMMatrix * D;
@@ -75,4 +72,16 @@ void main() {
   gl_Position = PCMMatrix * E;
   EmitVertex();
   EndPrimitive();
+}
+
+float rand(vec2 co){
+  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+vec4 bary(float R, float S) {
+  if (R + S >= 1) {
+    R = 1 - R;
+    S = 1 - S;
+  }
+  return gl_in[0].gl_Position + (R * (gl_in[1].gl_Position - gl_in[0].gl_Position)) + (S * (gl_in[2].gl_Position - gl_in[0].gl_Position));
 }

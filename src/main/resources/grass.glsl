9d13e1cd16b053fa55cd475d1efce7c0a01f3d2d
Ryan Plessner <plessner.ryan@gmail.com>
2014-02-16 13:11:13 -0500

    added the canonical glsl random function and gave the grass blade a little dimensionality

--- src/main/resources/grass.glsl
+++ src/main/resources/grass.glsl
@@ -7,9 +7,13 @@ uniform mat4 modelMatrix;
 uniform mat4 cameraMatrix;
 uniform mat4 perspectiveMatrix;
 
-layout(triangle_strip, max_vertices=6) out;
+layout(triangle_strip, max_vertices=15) out;
 
 out vec4 gColor;
+
+float rand(vec2 co){
+  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
 
 vec4 bary(float R, float S) {
   if (R + S >= 1) {
@@ -20,7 +24,9 @@ vec4 bary(float R, float S) {
 }
 
 void main() {
-  mat4 PCMMatrix = (perspectiveMatrix * cameraMatrix) * modelMatrix;
+  mat4 PCMMatrix = (perspectiveMatrix * cameraMatrix) * modelMatrix; // Precompute the perspective/camera/model matrix
+
+  // Emit the original triangle
   for (int i = 0; i < gl_in.length(); i++) {
     gl_Position = PCMMatrix * gl_in[i].gl_Position;
     gColor = vColor[i];
@@ -28,16 +34,45 @@ void main() {
   }
   EndPrimitive();
 
-  vec4 coordinate = bary(0.5, 0.5);
+  vec4 coordinate = bary(rand(vec2(40, 20)), rand(vec2(0.4, 0.2)));
+  vec4 A = coordinate + vec4(0, 0, 0.01, 0);
+  vec4 B = coordinate + vec4(0.0025, 0, 0, 0);
+  vec4 C = coordinate - vec4(0, 0, 0.01, 0);
+  vec4 D = coordinate - vec4(0.0025, 0, 0, 0);
+  vec4 E = coordinate + vec4(0, 0.1, 0, 0);
 
   // Emit coordinates
   gColor = vColor[0] + vec4(0.4, 0, 0, 0);
   
-  gl_Position = PCMMatrix * coordinate;
+  gl_Position = PCMMatrix * A;
   EmitVertex();
-  gl_Position = PCMMatrix * (coordinate - vec4(0.01, 0, 0, 0));
+  gl_Position = PCMMatrix * D;
   EmitVertex();
-  gl_Position = PCMMatrix * (coordinate + vec4(-0.005, 0.1, 0, 0));
+  gl_Position = PCMMatrix * E;
+  EmitVertex();
+  EndPrimitive();
+
+  gl_Position = PCMMatrix * A;
+  EmitVertex();
+  gl_Position = PCMMatrix * B;
+  EmitVertex();
+  gl_Position = PCMMatrix * E;
+  EmitVertex();
+  EndPrimitive();
+
+  gl_Position = PCMMatrix * C;
+  EmitVertex();
+  gl_Position = PCMMatrix * D;
+  EmitVertex();
+  gl_Position = PCMMatrix * E;
+  EmitVertex();
+  EndPrimitive();
+
+  gl_Position = PCMMatrix * C;
+  EmitVertex();
+  gl_Position = PCMMatrix * B;
+  EmitVertex();
+  gl_Position = PCMMatrix * E;
   EmitVertex();
   EndPrimitive();
 }
